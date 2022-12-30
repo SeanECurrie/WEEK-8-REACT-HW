3e90b067beda1cdf6ee948699f8989ba2f993b14
SeanECurrie <101378600+SeanECurrie@users.noreply.github.com>
2022-12-30 15:02:12 -0600

    Update MyCustomTable.jsx

--- src/components/MyCustomTable.jsx
+++ src/components/MyCustomTable.jsx
@@ -19,8 +19,8 @@ function createData(
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Dart Daddies", 4, 2, 6, '-'),
+  createData("Bullseye Boys", 3, 3, 6, '-1'),
   createData("Eclair", 262, 16.0, 24, 6.0),
   createData("Cupcake", 305, 3.7, 67, 4.3),
   createData("Gingerbread", 356, 16.0, 49, 3.9),
@@ -39,11 +39,11 @@ export default class MyCustomButton extends Component {
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell>Dessert</TableCell>
-                <TableCell align="right">Calories</TableCell>
-                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                <TableCell>Teams</TableCell>
+                <TableCell align="right">Wins</TableCell>
+                <TableCell align="right">Loses</TableCell>
+                <TableCell align="right">Matches</TableCell>
+                <TableCell align="right">Diff</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
